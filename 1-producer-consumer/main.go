0a04eb00b97ed85a64e91e9c0e9c1ee5e0e92b29
David Ellis <ellisda@gmail.com>
2018-09-16 10:01:48 -0600

    add channel

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -13,19 +13,24 @@ import (
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
-	for {
-		tweet, err := stream.Next()
-		if err == ErrEOF {
-			return tweets
-		}
+func producer(stream Stream) chan *Tweet {
+	s := make(chan *Tweet)
+	go func() {
+		for {
+			tweet, err := stream.Next()
+			if err == ErrEOF {
+				close(s)
+				return
+			}
 
-		tweets = append(tweets, tweet)
-	}
+			s <- tweet
+		}
+	}()
+	return s
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(tweets chan *Tweet) {
+	for t := range tweets {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
